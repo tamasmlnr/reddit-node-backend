2575322245e16a066896278b746b282d78111daf
Tamás <molnart90@gmail.com>
2019-08-08 11:28:47 +0200

    Add put to postRouter

--- controllers/posts.js
+++ controllers/posts.js
@@ -19,10 +19,32 @@ postRouter.get('/:id', async (request, response) => {
     .catch(error => response.status(400).status)
 })
 
+postRouter.put('/:id', async (request, response) => {
+  const body = request.body
 
+  const post = {
+    title: body.title,
+    author: body.author,
+    content: body.content,
+    score: body.score
+  }
+  // Post.findByIdAndUpdate(request.params.id, post, { new: true }).then(
+  //   updatedPost=>response.status(201).json(updatedPost.toJSON))
+  //   .catch(error =>console.log(error))
+
+  Post.findByIdAndUpdate(request.params.id, post, { new: true })
+    .then(post => {
+      if (post) {
+        response.json(post.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => response.status(400).status)
+
+})
 
 postRouter.post('/', async (request, response) => {
-
   try {
     const body = request.body
 
